a2e9ba2ddf4b4cbd13821dd42f2fe223941476af
Vlados5808 <vvladeslav70@gmail.com>
2024-09-11 17:27:09 +0300

    1.js

--- 1.js
+++ 1.js
@@ -1,6 +1,18 @@
-  function getDifference() {
+  //  1
+const num1 = prompt("Введіть перше число:");
+const num2 = prompt("Введіть друге число:");
+let d = num1 - num2;
+alert("Різниця між числами: " + d);
+//  2
+function getMax () {
     const num1 = prompt("Введіть перше число:");
     const num2 = prompt("Введіть друге число:");
-     let difference = num1 - num2;
-    alert("Різниця між числами: " + d);
-  }
+    if (num1 > num2) {
+        alert("Перше число більше: " + num1);
+    } else {
+        alert("Друге число більше: " + num2);
+    }
+}
+getMax();
+// 3
+alert("hello world");
