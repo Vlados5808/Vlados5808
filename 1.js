209efe3f8e5df422c69da8ce5c66667c14d857a7
Vlados5808 <vvladeslav70@gmail.com>
2024-09-11 16:55:21 +0300

    Update and rename README.js to 1.js

---
+++ 1.js
@@ -0,0 +1,6 @@
+  function getDifference() {
+    const num1 = prompt("Введіть перше число:");
+    const num2 = prompt("Введіть друге число:");
+     let difference = num1 - num2;
+    alert("Різниця між числами: " + d);
+  }
